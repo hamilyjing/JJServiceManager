a4155f4c9b7e93f1f70b40bab5a8edc19ed3ebd8
JJ <gongjian_001@126.com>
2018-01-09 10:21:45 +0800

    up

---
+++ JJServiceManager/JJServiceManager.h
@@ -0,0 +1,19 @@
+//
+//  JJServiceManager.h
+//  JJServiceManager
+//
+//  Created by 宫健(金融壹账通客户端研发团队) on 09/01/2018.
+//  Copyright © 2018 宫健(金融壹账通客户端研发团队). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJServiceManager.
+FOUNDATION_EXPORT double JJServiceManagerVersionNumber;
+
+//! Project version string for JJServiceManager.
+FOUNDATION_EXPORT const unsigned char JJServiceManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJServiceManager/PublicHeader.h>
+
+
