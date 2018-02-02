be92a9bb73aa2a7663a4279addf35a387f6d1d44
JJ <gongjian_001@126.com>
2018-02-02 10:38:12 +0800

    UP

--- JJServiceManager/ServiceFactory/JJServiceFactory.h
+++ JJServiceManager/ServiceFactory/JJServiceFactory.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 /**
- *  Service factory manages all service, include loading and unloading service. You can use jj_SERVICE macro to get specific service.
+ *  Service factory manages all service, include loading and unloading service. You can use JJ_SERVICE macro to get specific service.
  */
 @interface JJServiceFactory : NSObject
 
@@ -30,12 +30,12 @@
 
 #pragma mark - Service Macro
 
-#define jj_SERVICE(service) ((service *)[[JJServiceFactory sharedServiceFactory] serviceWithServiceName:[service serviceName]])
+#define JJ_SERVICE(service) ((service *)[[JJServiceFactory sharedServiceFactory] serviceWithServiceName:[service serviceName]])
 
-#define jj_UNLOAD_SERVICE(service) [[JJServiceFactory sharedServiceFactory] unloadServiceWithServiceName:[service serviceName]]
+#define JJ_UNLOAD_SERVICE(service) [[JJServiceFactory sharedServiceFactory] unloadServiceWithServiceName:[service serviceName]]
 
 #pragma mark - Feature Set Macro
 
-#define jj_FEATURE_SET(service, featureSet) ((featureSet *)[[[JJServiceFactory sharedServiceFactory] serviceWithServiceName:[service serviceName]] featureSetWithFeatureSetName:[featureSet featureSetName]])
+#define JJ_FEATURE_SET(service, featureSet) ((featureSet *)[[[JJServiceFactory sharedServiceFactory] serviceWithServiceName:[service serviceName]] featureSetWithFeatureSetName:[featureSet featureSetName]])
 
-#define jj_UNLOAD_FEATURE_SET(service, featureSet) [[[JJServiceFactory sharedServiceFactory] serviceWithServiceName:[service serviceName]] unloadFeatureSetWithFeatureSetName:[featureSet featureSetName]]
+#define JJ_UNLOAD_FEATURE_SET(service, featureSet) [[[JJServiceFactory sharedServiceFactory] serviceWithServiceName:[service serviceName]] unloadFeatureSetWithFeatureSetName:[featureSet featureSetName]]
